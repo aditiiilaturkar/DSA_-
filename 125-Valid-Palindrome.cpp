2181b1a75447054f2ddac456a419756aff6d43c3
Aditi Laturkar <32028042+aditiiilaturkar@users.noreply.github.com>
2024-10-10 11:28:20 -0400

    [125] [Time Beats: 79.96%] [Memory Beats: 53.61%] - LeetPush

---
+++ 125-Valid-Palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(), s.end(), s.begin(), ::tolower); 
+       
+        int i=0, j = s.length()-1;
+       
+        while(i<=j){
+            while(i<j && !isalnum(s[i])) i++;
+            while(i<j && !isalnum(s[j])) j--;
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
