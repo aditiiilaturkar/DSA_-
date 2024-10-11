81572420716137618c25a5b7d330ee64507e7238
Aditi Laturkar <32028042+aditiiilaturkar@users.noreply.github.com>
2024-10-11 02:02:07 -0400

    [938] [Time Beats: 17.23%] [Memory Beats: 96.95%] - LeetPush

--- 938-Range-Sum-of-BST.cpp
+++ 938-Range-Sum-of-BST.cpp
@@ -11,16 +11,12 @@
  */
 class Solution {
 public:
-    int dfs(TreeNode* root, int low, int high){
-        if(!root) return 0;
-        if(root->val <= high && root->val>= low){
-            return root->val + dfs(root->left, low, high) + dfs(root->right, low, high);
-        }else {
-            return dfs(root->left, low, high) + dfs(root->right, low, high);
-        }
-        return 0;
-    }
+    int ans = 0;
     int rangeSumBST(TreeNode* root, int low, int high) {
-       return dfs(root, low, high);
+        if(!root) return ans;
+        if(root->val<= high && root->val>=low) ans+= root->val;
+        if(root->left) rangeSumBST(root->left, low, high);
+        if(root->right) rangeSumBST(root->right, low, high);
+        return ans;  
     }
 };
