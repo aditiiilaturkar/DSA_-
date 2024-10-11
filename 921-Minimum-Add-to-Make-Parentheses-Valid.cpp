438bd2e661a16b8062fb8304b166925e462157e6
Aditi Laturkar <32028042+aditiiilaturkar@users.noreply.github.com>
2024-10-11 01:49:06 -0400

    [921] [Time Beats: 17.72%] [Memory Beats: 22.43%] - LeetPush

--- 921-Minimum-Add-to-Make-Parentheses-Valid.cpp
+++ 921-Minimum-Add-to-Make-Parentheses-Valid.cpp
@@ -3,17 +3,18 @@ public:
     int minAddToMakeValid(string s) {
         stack<char>st;
         st.push(s[0]);
-        for(int i=1; i< s.length(); i++){
-            if(s[i] ==')') {
-                if(!st.empty() && st.top() == '('){
+        int i=1; 
+        while(i<s.length()){
+            if(s[i] == ')') {
+                if(!st.empty() && st.top() == '(') {
                     st.pop();
-                    continue;
                 }else{
                     st.push(s[i]);
                 }
             }else{
                 st.push(s[i]);
             }
+            i++;
         }
         return st.size();
     }
