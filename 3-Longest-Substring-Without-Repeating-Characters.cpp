314c584aa9eb3ebdaeec5a000a54f52807fe068e
Aditi Laturkar <32028042+aditiiilaturkar@users.noreply.github.com>
2024-10-13 00:29:28 -0400

    [3] [Time Beats: 87.48%] [Memory Beats: 74.62%] - LeetPush

---
+++ 3-Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int>v(256, -1);
+        int start =-1;
+        int ans = 0;
+        for(int i=0;i <s.length(); i++){
+            if(v[s[i]] > start){
+                start = v[s[i]];
+            }
+            v[s[i]] = i; 
+            ans = max(ans, i-start);
+        }
+        return ans;
+    }
+};
