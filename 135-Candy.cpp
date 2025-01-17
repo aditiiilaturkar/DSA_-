1a98e07c0e98a89e4287a977817dc0fa34a0eb09
Aditi Laturkar <32028042+aditiiilaturkar@users.noreply.github.com>
2025-01-17 18:10:39 +0530

    [135] [Time Beats: 100.00%] [Memory Beats: 40.08%] - LeetPush

--- 135-Candy.cpp
+++ 135-Candy.cpp
@@ -1,23 +1,53 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int ans = 0;
-        vector<int>can(ratings.size(), 1);
-        for(int i=1; i<ratings.size(); i++){
-            if(ratings[i]>ratings[i-1]) {   //check left neighbour when coming left to right
-                can[i] = 1+can[i-1];
+        int n = ratings.size();
+        int candy = n;
+        int i=1;
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
             }
-        }
-        for(int i=ratings.size()-2;i>=0; i--){
-            if(ratings[i]>ratings[i+1] ){   //check right neighbour when coming right to left
-                can[i]=max(can[i], can[i+1] + 1);
+            // pahad chad rhe hai, increaing slope
+            int peak =0;
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak++;
+                candy += peak;
+                i++;
             }
-        }
+            // pahad utar rhe hai, decreasing slope
+            int dip=0;
+            while(i<n && ratings[i] < ratings[i-1]){
+                dip++;
+                candy += dip;
+                i++;
+            }
 
-        for(int i=0;i<can.size();i++){
-            ans+=can[i];
+            candy -= min(dip, peak);
         }
-        return ans;
+        return candy;
+
+
+
+
+        // int ans = 0;
+        // vector<int>can(ratings.size(), 1);
+        // for(int i=1; i<ratings.size(); i++){
+        //     if(ratings[i]>ratings[i-1]) {   //check left neighbour when coming left to right
+        //         can[i] = 1+can[i-1];
+        //     }
+        // }
+        // for(int i=ratings.size()-2;i>=0; i--){
+        //     if(ratings[i]>ratings[i+1] ){   //check right neighbour when coming right to left
+        //         can[i]=max(can[i], can[i+1] + 1);
+        //     }
+        // }
+
+        // for(int i=0;i<can.size();i++){
+        //     ans+=can[i];
+        // }
+        // return ans;
     }
 
     
